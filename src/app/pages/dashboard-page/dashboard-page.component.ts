83df350afd213cb2a0a7339dc7dfc6fd2d3a32af
Wiky Lyu <wiiiky@outlook.com>
2021-11-27 12:27:25 +0800

    学生和老师数量

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.scss'],
 })
 export class DashboardPageComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  self: any = {};
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.getSelf();
+  }
+
+  async getSelf() {
+    try {
+      const r = await this.api.getSelf();
+      this.self = r.data;
+    } catch (error) {
+
+    }
+  }
 }
