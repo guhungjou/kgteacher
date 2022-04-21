ca7b57fdfba0373793083a108ff8bb6758a0f519
Wiky Lyu <wiiiky@outlook.com>
2022-04-21 09:46:39 +0800

    年龄

--- src/app/kindergarten/components/student-age/student-age.component.ts
+++ src/app/kindergarten/components/student-age/student-age.component.ts
@@ -20,10 +20,22 @@ export class StudentAgeComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.dt = new Date(this.birthday);
     if (this.dt) {
-      const m = moment(this.dt);
-      const now = moment();
-      this.years = now.diff(m, 'years');
-      this.months = now.diff(m, 'months') % 12;
+      // const m = moment(this.dt);
+      // const now = moment();
+      // this.years = now.diff(m, 'years');
+      // this.months = now.diff(m, 'months') % 12;
+      const now = new Date();
+      let year = now.getFullYear() - this.dt.getFullYear();
+      let month = now.getMonth() - this.dt.getMonth();
+      let day = now.getDate() - this.dt.getDate();
+      month = year * 12 + month;
+      if (day >= 15) {
+        month += 1;
+      } else if (day < -15) {
+        month -= 1;
+      }
+      this.years = Math.floor(month / 12);
+      this.months = month % 12;
     }
   }
 
