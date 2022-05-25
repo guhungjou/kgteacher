86645b9471c0ac29c99c0aaae76beb9c2a84ce6e
Wiky Lyu <wiiiky@outlook.com>
2022-05-25 09:02:45 +0800

    kgteacher

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/teacher'),
+      dir: require('path').join(__dirname, './coverage/kgteacher'),
       subdir: '.',
       reporters: [
         { type: 'html' },
